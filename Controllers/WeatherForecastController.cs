2b1df2acf6a69d77fa4ef981eeb5852ddb857ff4
imovila <ion.movila@gmail.com>
2024-02-29 10:38:59 -0500

    Changed controllers.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "MYX"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "MYXY"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
